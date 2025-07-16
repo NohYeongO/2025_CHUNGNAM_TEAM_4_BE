df5c3bd08a789f5b672a954996024cad00f4c926
skxcv312 <skxcv322@google.com>
2025-07-17 00:30:22 +0900

    feat: ErrorCode에 INVALID_CHALLENGE 항목 추가
    - challenge가 존재하지 않을 때의 에러 코드 정의

--- src/main/java/com/chungnam/eco/common/exception/ErrorCode.java
+++ src/main/java/com/chungnam/eco/common/exception/ErrorCode.java
@@ -10,7 +10,10 @@ public enum ErrorCode {
     
     // 일반적인 에러
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S001", "서버 내부 오류가 발생했습니다."),
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "S002", "잘못된 요청입니다.");
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "S002", "잘못된 요청입니다."),
+
+    // challenge가 존재하지 않을 때 에러
+    INVALID_CHALLENGE(HttpStatus.INTERNAL_SERVER_ERROR,"S003","challenge가 존재하지 않습니다.");
     
     private final HttpStatus httpStatus;
     private final String code;
