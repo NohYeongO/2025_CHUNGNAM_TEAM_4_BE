38a5e79a00795daf2a2f62230906d1149d305eb6
skxcv312 <skxcv322@google.com>
2025-07-16 22:59:37 +0900

    feat: Mission 엔티티 추가
    - BaseTimeEntity를 상속받아 시간 자동 기록
    - 기본 생성자를 protected로 설정해 빌더 패턴으로만 객체 생성 가능하도록 강제

--- src/main/java/com/chungnam/eco/mission/domain/Mission.java
+++ src/main/java/com/chungnam/eco/mission/domain/Mission.java
@@ -1,4 +1,54 @@
 package com.chungnam.eco.mission.domain;
 
-public class Mission {
+import com.chungnam.eco.common.entity.BaseTimeEntity;
+import jakarta.persistence.Access;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "mission")
+public class Mission extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Lob
+    @Column(name = "description",nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false, length = 20)
+    private MissionType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 20)
+    private MissionStatus status;
+
+    @Column(name = "reward_points", nullable = false)
+    private Integer rewardPoints;
+
+    @Builder
+    public Mission(String title, String description, MissionType type, MissionStatus status, Integer rewardPoints) {
+        this.title = title;
+        this.description = description;
+        this.type = type;
+        this.status = status;
+        this.rewardPoints = rewardPoints;
+    }
 }
