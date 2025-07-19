7d4d8d0c27be3b8efa361426cb3ce17465341d36
skxcv312 <skxcv322@google.com>
2025-07-20 02:33:59 +0900

    feat: MissionService에 상태값 변환 및 조회 기능 추가
    - String 상태값을 MissionStatus enum으로 변환하는 toMissionStatus 메서드 구현 (소문자 허용)
    - 상태값으로 미션 목록을 조회하는 findMissionList 메서드 추가
    - activateMission / deactivateMission 메서드에 상태 변경 로직 포함
    - 미션 조회 시 존재하지 않으면 InvalidMissionException을 발생시키도록 findMissionById 구현
    - ErrorCode에 INVALID_MISSION 에러코드(E002) 추가

--- src/main/java/com/chungnam/eco/common/exception/ErrorCode.java
+++ src/main/java/com/chungnam/eco/common/exception/ErrorCode.java
@@ -7,14 +7,15 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    
+
     // 일반적인 에러
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S001", "서버 내부 오류가 발생했습니다."),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "S002", "잘못된 요청입니다."),
 
-    // challenge가 존재하지 않을 때 에러
-    INVALID_CHALLENGE(HttpStatus.INTERNAL_SERVER_ERROR,"S003","challenge가 존재하지 않습니다."),
-
+    // 엔티티 관련 에러
+    INVALID_CHALLENGE(HttpStatus.INTERNAL_SERVER_ERROR, "E001", "Challenge가 존재하지 않습니다."),
+    INVALID_MISSION(HttpStatus.INTERNAL_SERVER_ERROR, "E002", "Mission이 존재하지 않습니다."),
+    
     // 사용자 관련 에러
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "사용자를 찾을 수 없습니다."),
 
