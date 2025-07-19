8cfe242536fcf131c24dc44084d32b8fded64173
노영오 <nohyo05@naver.com>
2025-07-20 00:30:30 +0900

    refactor: UserMission 도메인 패키지 이동 및 구조 개선
    -  패키지에 있던  도메인 및 리포지토리를  패키지로 이동했습니다.
    - 도메인의 응집도를 높이고 패키지 구조를 명확하게 개선하고자 했습니다.

--- src/main/java/com/chungnam/eco/user/domain/UserMissionStatus.java
+++
@@ -1,8 +0,0 @@
-package com.chungnam.eco.user.domain;
-
-public enum UserMissionStatus {
-    NOT_STARTED,  // 시작 안함
-    IN_PROGRESS,  // 진행 중
-    SUBMITTED,    // 제출됨
-    COMPLETED     // 완료됨
-}
