7fc96023b3156b5da0c05807228f4a4aa906f8dd
skxcv312 <skxcv322@google.com>
2025-07-21 05:12:12 +0900

    feat: User 포인트 지급 메서드 및 ChallengeStatus 설명 추가
    - User 엔티티에 SupplyPoint(Integer point) 메서드 추가로 포인트 적립 기능 구현
    - ChallengeStatus enum에 description 필드와 상태 설명값 추가

--- src/main/java/com/chungnam/eco/challenge/domain/ChallengeStatus.java
+++ src/main/java/com/chungnam/eco/challenge/domain/ChallengeStatus.java
@@ -1,8 +1,17 @@
 package com.chungnam.eco.challenge.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum ChallengeStatus {
-    PENDING,
-    REJECTED,
-    IN_PROGRESS,
-    COMPLETED
+    PENDING("요청"),
+    REJECTED("거절"),
+    IN_PROGRESS("진행중"),
+    COMPLETED("승인");
+
+    private final String description;
+
+    ChallengeStatus(String description) {
+        this.description = description;
+    }
 }
