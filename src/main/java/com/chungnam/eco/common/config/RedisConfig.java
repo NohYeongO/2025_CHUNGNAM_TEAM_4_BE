a235b55b218def505f8a8184a54aeb767f41a1ac
노영오 <nohyo05@naver.com>
2025-07-23 01:56:00 +0900

    refactor: redis password 설정

--- src/main/java/com/chungnam/eco/common/config/RedisConfig.java
+++ src/main/java/com/chungnam/eco/common/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -23,9 +24,19 @@ public class RedisConfig {
     @Value("${redis.main.port}")
     private int redisPort;
 
+    @Value("${redis.main.password:}")
+    private String redisPassword;
+
     @Bean(name = "redisConnectionFactory")
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
+        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+                .useSsl()
+                .build();
+
+        RedisStandaloneConfiguration serverConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        serverConfiguration.setPassword(redisPassword);
+
+        return new LettuceConnectionFactory(serverConfiguration, clientConfiguration);
     }
 
     @Bean(name = "redisTemplate")
