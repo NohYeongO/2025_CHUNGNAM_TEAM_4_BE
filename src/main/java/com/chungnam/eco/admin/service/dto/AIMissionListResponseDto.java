cbc1c858ff462f21c076584952378438c7533638
skxcv312 <skxcv322@google.com>
2025-07-26 18:45:59 +0900

    fix: AI 미션 생성후 json 파싱 오류 해결
    - @Builder만 있는 경우 Jackson은 역직렬화를 실패함
    - @NoArgsConstructor, @AllArgsConstructor 어노테이션을 추가하여 json 파싱 오류 해결

--- src/main/java/com/chungnam/eco/admin/service/dto/AIMissionListResponseDto.java
+++ src/main/java/com/chungnam/eco/admin/service/dto/AIMissionListResponseDto.java
@@ -3,20 +3,27 @@ package com.chungnam.eco.admin.service.dto;
 import com.chungnam.eco.mission.domain.MissionCategory;
 import com.chungnam.eco.mission.domain.MissionLevel;
 import com.chungnam.eco.mission.domain.MissionType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // 전체 미션 리스트 응답을 감싸는 DTO
-@Getter
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AIMissionListResponseDto {
     private List<AIMissionDto> missions;
 
 
-    @Getter
+    @Data
     @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class AIMissionDto {
         @JsonPropertyDescription("미션의 제목을 지정")
         private String title;
@@ -25,12 +32,15 @@ public class AIMissionListResponseDto {
         private String description;
 
         @JsonPropertyDescription("미션의 타입 지정")
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         private MissionType type;
 
         @JsonPropertyDescription("미션의 카테고리 지정")
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         private MissionCategory category;
 
         @JsonPropertyDescription("미션의 레벨 중 타입과 미션의 난이도를 선정하여 선택")
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         private MissionLevel level;
 
         @JsonPropertyDescription("미션 완료 시 지급할 보상 포인트, "
