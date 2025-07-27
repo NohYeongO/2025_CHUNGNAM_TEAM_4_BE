2065f4b4662eb0d6fe5f6d6776856642835ee37c
skxcv312 <skxcv322@google.com>
2025-07-27 16:39:34 +0900

    refactor: Challenge 조회 응답 값에 create_at 추가
    - ChallengeDetailResponse,AllChallengeResponse 생성 날짜를 추가함

--- src/main/java/com/chungnam/eco/admin/controller/response/AllChallengeResponse.java
+++ src/main/java/com/chungnam/eco/admin/controller/response/AllChallengeResponse.java
@@ -1,22 +1,43 @@
 package com.chungnam.eco.admin.controller.response;
 
 import com.chungnam.eco.admin.service.dto.ChallengeDto;
+import com.chungnam.eco.mission.domain.Mission;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
 
-@Builder
 @Getter
+@Builder
 public class AllChallengeResponse {
-    private final List<ChallengeResponse> challenge_list;
 
-    @Builder
+    private final List<ChallengeResponse> challengeList;
+
     @Getter
-    private static class ChallengeResponse {
+    @Builder
+    public static class ChallengeResponse {
         private Long challenge_id;
         private String user_nickname;
-        private String mission_title;
+        private MissionInfo mission_info;
+        private LocalDateTime created_at;
         private String status;
+    }
+
+    @Getter
+    @Builder
+    public static class MissionInfo {
+        private String title;
+        private String type;
+        private String category;
+
+        public static MissionInfo from(Mission mission) {
+            return MissionInfo.builder()
+                    .title(mission.getTitle())
+                    .type(mission.getType().name())
+                    .category(mission.getCategory().getDescription())
+                    .build();
+        }
     }
 
     public static AllChallengeResponse success(List<ChallengeDto> challengeDtoList) {
@@ -24,13 +45,14 @@ public class AllChallengeResponse {
                 .map(dto -> ChallengeResponse.builder()
                         .challenge_id(dto.getId())
                         .user_nickname(dto.getUser().getNickname())
-                        .mission_title(dto.getMission().getTitle())
+                        .mission_info(MissionInfo.from(dto.getMission()))
+                        .created_at(dto.getCreatedAt())
                         .status(dto.getChallengeStatus().getDescription())
                         .build())
-                .toList();
+                .collect(Collectors.toList());
 
         return AllChallengeResponse.builder()
-                .challenge_list(responses)
+                .challengeList(responses)
                 .build();
     }
 }
